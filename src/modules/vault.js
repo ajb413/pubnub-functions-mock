069bea0f94199c5b97e45537646df2864e4d6ed9
Adam Bavosa <adamb@pubnub.com>
2018-05-02 13:56:50 -0700

    added vault get test

--- src/modules/vault.js
+++ src/modules/vault.js
@@ -2,7 +2,7 @@ const vaultInterface = {
   get: (key) => {
     return new Promise((resolve, reject) => {
       if (typeof key !== 'string' || key === '') {
-        reject(`Invalid key ${key}, non-empty string required`);
+        reject(`Invalid key [${key}], non-empty string required`);
       }
 
       resolve(key);
--- test/unit.test.js
+++ test/unit.test.js
@@ -873,7 +873,27 @@ describe('#endpoint', () => {
     };
 
     let correctResult = {
-      'body': 'Invalid key undefined, non-empty string required',
+      'body': 'Invalid key [undefined], non-empty string required',
+      'status': 500,
+    };
+
+    endpoint(request, response).then((testResult) => {
+      assert.equal(testResult.status, correctResult.status, 'status');
+      assert.equal(testResult.body, correctResult.body, 'body');
+      done();
+    });
+  });
+
+  it('fails vault.get for empty string', function(done) {
+    let request = Object.assign({}, endpointRequestObject);
+    let response = Object.assign({}, endpointResponseObject);
+
+    request.vault = {
+      key: '',
+    };
+
+    let correctResult = {
+      'body': 'Invalid key [], non-empty string required',
       'status': 500,
     };
 
